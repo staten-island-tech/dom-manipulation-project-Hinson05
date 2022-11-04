b2684cc5c866dc37eb2d64cebad440eb90bb0f83
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2022-11-04 13:59:13 -0400

    a

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const DomSelectors = {
-    button: document.getElementById("buton");
-    text: document.querySelector("#text");
+  button: document.getElementById("buton"),
+  text: document.querySelector("#text"),
+  point: document.querySelectorAll(".point"),
+  pointTwo: document.getElementById("pointTwo"),
+};
+console.log(DomSelectors.pointTwo);
+function backgroundAndText(background, text) {
+  background.style.backgroundColor = "red";
+  text.innerHTML; = "This is now a big red box";
+  text.style.fontSize= "40px";
 }
-console.log(DomSelectors.button);
-
