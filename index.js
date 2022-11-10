051f3e82aaf68c7d4e227c1808ddd3b8c542db3e
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2022-11-10 14:01:18 -0500

    a

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 const DomSelectors = {
   button: document.getElementById("buton"),
-  text: document.querySelector("#text"),
-  point: document.querySelectorAll(".point"),
-  pointTwo: document.getElementById("pointTwo"),
+  text: document.getElementById("container-box"),
+  input: document.querySelector( '#input' ),
 };
+DomSelectors.box.insertAdjacentHTML("beforeend", '<h1>Contact Saver</h1>');
+
 console.log(DomSelectors.pointTwo);
 function backgroundAndText(background, text) {
-  background.style.backgroundColor = "red";
-  text.innerHTML; = "This is now a big red box";
+  background.style.backgroundColor = "black";
+  text.innerHTML; = "Contact Saver";
   text.style.fontSize= "40px";
 }
 backgroundAndText(DomSelectors.box, DomSelectors.text);
 
 DomSelectors.button.addEventListener("click", function(){
-  backgroundAndText
+  backgroundAndText(DomSelectors.box, DomSelectors.text);
 }
+function saveCon() {
+  DomSelectors.point.forEach(point) => {
+    point.addEventListener("click", function(){
+      point.textContent= "Hello"; 
+    }
+
 
 //delete cared
 //querySelectorAll 
