30508e897680a0864cf3e62c9f8dcdf48f8bbdd2
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2022-11-15 13:59:17 -0500

    a

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ console.log("name");
 let num = document.getElementById("input2").value;
 console.log("num");
 
-DomSelectors.button.addEventListener("Add", function () {
+DomSelectors.button.addEventListener("Submit", function () {
   let name = DomSelectors.name.value;
-  DomSelectors.box.insertAdjacentHTML("afterend", <p>${name}</p>)
-}
+  DomSelectors.box.insertAdjacentHTML("afterend", <p>${name}</p>);
+});
 
-DomSelectors.button.addEventListener("Add", function () {
+DomSelectors.button.addEventListener("Submit", function () {
   let num = DomSelectors.num.value;
-  DomSelectors.box.insertAdjacentHTML("afterend", <p>${num}</p>)
-}
-
-
+  DomSelectors.box.insertAdjacentHTML("afterend", <p>${num}</p>);
+});
