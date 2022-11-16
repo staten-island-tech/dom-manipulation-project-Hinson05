c2f35cee0d8446ee8799a30f4b3ea97c35b0588a
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2022-11-16 14:00:25 -0500

    a

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const DomSelectors = {
   input: document.querySelector("#input"),
 };
 
-let name = document.getElementById("input").value;
+let name = document.getElementById("name").value;
 console.log("name");
 
-let num = document.getElementById("input2").value;
+let num = document.getElementById("num").value;
 console.log("num");
 
 DomSelectors.button.addEventListener("Submit", function () {
@@ -19,3 +19,5 @@ DomSelectors.button.addEventListener("Submit", function () {
   let num = DomSelectors.num.value;
   DomSelectors.box.insertAdjacentHTML("afterend", <p>${num}</p>);
 });
+
+let list = document.querySelector();
