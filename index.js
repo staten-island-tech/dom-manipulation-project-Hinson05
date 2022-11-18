8f7178e930d286665ad59b9e558bfb39b1555a0a
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2022-11-18 10:25:48 -0500

    a

--- index.js
+++ index.js
@@ -2,21 +2,17 @@ const DOMSelectors = {
   submit: document.getElementById("submit"),
   list: document.getElementById("list"),
   name: document.getElementById("name"),
-  clear: document.getElementById("clear")
+  clear: document.getElementById("clear"),
 };
 
-
 DOMSelectors.submit.addEventListener("click", function () {
-  let x = DOMSelectors.name.value
-  DOMSelectors.list.insertAdjacentHTML('afterbegin', <p>${x}</p>)
+  let x = DOMSelectors.name.value;
+  DOMSelectors.list.insertAdjacentHTML("afterbegin", `<p>${x}</p>`);
 });
 
-DOMSelectors.clear.addEventListener("click", function() {
+DOMSelectors.clear.addEventListener("click", function () {
   DOMSelectors.list.innerHTML = "";
 });
 
-
-DomSelectors.submit.addEventListener("click", function () {
-
-});
-console.log('test');
+DomSelectors.submit.addEventListener("click", function () {});
+console.log("test");
