9222acd417647176faad1a65ccde8f2cd3f5bc6d
Hinson05 <113132293+Hinson05@users.noreply.github.com>
2022-11-21 13:40:20 -0500

    a

--- index.js
+++ index.js
@@ -4,16 +4,35 @@ const DOMSelectors = {
   name: document.getElementById("name"),
   num: document.getElementById("num"),
   clear: document.getElementById("clear"),
+  delete: document.getElementById("delete"),
 };
 
 DOMSelectors.submit.addEventListener("click", function () {
   let e = DOMSelectors.num.value;
-  DOMSelectors.list.insertAdjacentHTML("afterbegin", `<p>${e}</p>`);
+  DOMSelectors.list.insertAdjacentHTML(
+    "afterbegin",
+    `<p class= "yes">${e}</p>`
+  );
+
+  DOMSelectors.num.value = "";
+
   let x = DOMSelectors.name.value;
-  DOMSelectors.list.insertAdjacentHTML("afterbegin", `<p>${x}</p>`);
+  DOMSelectors.list.insertAdjacentHTML(
+    "afterbegin",
+    `<p class= "yes" >${x}</p>`
+  );
+
+  DOMSelectors.name.value = "";
 });
 
 DOMSelectors.clear.addEventListener("click", function () {
   DOMSelectors.list.innerHTML = "";
 });
 
+DOMSelectors.delete.addEventListener("click", function () {
+  let wowow = document.getElementsByClassName("yes");
+  let wowie = wowow.item(0);
+  let wowie2 = wowow.item(1);
+  wowie.remove();
+  wowie2.remove();
+});
